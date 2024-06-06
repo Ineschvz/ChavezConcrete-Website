cdff037902fec0323137785832b8baacfc441e66
Ines Chavez <ineschvz21@gmail.com>
2024-06-06 10:02:26 -0700

    edited hompage

--- chavezconcrete-page/src/app/components/homepage-slides.js
+++ chavezconcrete-page/src/app/components/homepage-slides.js
@@ -1,12 +1,35 @@
-import React from "react"
-import Swiper from 'swiper';
+"use client";
+
+import React, { useRef, useState } from "react"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// import './styles.css';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
 
 
 const Slides = () => {
     return (
-<div> 
-  here i will insert slides using swipper.js
-</div>
+        <>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper ">
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </>
     )
 }
 
