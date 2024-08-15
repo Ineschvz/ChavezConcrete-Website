2d817d1dff53891f925f052547d528a4128a2466
Ines Chavez <ineschvz21@gmail.com>
2024-08-15 19:07:42 -0400

    added watermark to homepage slides

--- chavezconcrete-page/src/app/components/Slider.js
+++ chavezconcrete-page/src/app/components/Slider.js
@@ -1,82 +1,3 @@
-// 'use client';
-
-// import React, {useEffect, useState} from "react";
-// import Image from "next/image"; // Import the Image component from Next.js
-// import Description from "./description";
-// import { images } from "../../../utils/constants";
-
-
-// // import images from "../../../utils/constants";
-
-// const imageList = [
-//     {src: "/ImgSlide1.jpg", alt: "First Slide"},
-//     {src: "/Img2.jpg", alt: "Second Slide"},
-//     {src: "/Img3.jpg", alt: "Third Slide"},
-//     {src: "/DSC01509.jpg", alt: "Fourth SLide"},
-// ]
-
-
-// const Slider = () => {
-//     const[activeImage, setActiveImage] = useState(0);
-
-//     // const clickNext = () => {
-//     //     activeImage === images.length - 1
-//     //     ? setActiveImage(0)
-//     //     : setActiveImage(activeImage -1)
-//     // };
-//     const clickNext = () => {
-//         setActiveImage((prevActiveImage) => (prevActiveImage === imageList.length - 1 ? 0 : prevActiveImage + 1));
-//     };
-//     const clickPrev = () => {
-//         setActiveImage((prevActiveImage) => (prevActiveImage === 0 ? imageList.length - 1 : prevActiveImage - 1));
-//     };
-
-//     useEffect (()=> {
-
-//         const timer = setTimeout(() => {
-//            clickNext();
-//         }, 5000);
-//         return () =>{
-//             clearTimeout (timer);
-//         };
-
-//     }, [activeImage]);
-
-//     // const clickPrev = () => {
-//     //     activeImage === 0
-//     //     ? setActiveImage(images.length - 1 )
-//     //     : setActiveImage(activeImage -1 )
-//     // };
-//     return (
-//         <div className= " bg-white w-full max-w-6xl mx-auto py-12 md:py-20 grid place-items-center shadow-2xl">
-//             <div 
-//             className="w-full flex justify-center items-center gap-4 transition-transform ease-in-out duration-500 rounded-lg "
-//             >
-//                 {imageList.map((image,index)=>(
-//                     <div className= {`${ 
-//                         index === activeImage
-//                         ? "block w-full object-cover transition-all duration-500 ease-in-out"
-//                         : "hidden"
-                    
-//                     }`}>
-//                     <Image
-            
-//                     key={index} // Unique key for each image
-//                     src={image.src} // Source path of the image
-//                     alt={image.alt} // Alt text for the image
-//                     width={400} // Specify the width of the image
-//                     height={400}
-//                     className="w-full h-full object-cover rounded-lg flex justify-center items-center"
-//                     />
-//                     </div>
-//                 ))}
-        
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Slider 
 
 'use client';
 
@@ -124,6 +45,18 @@ const Slider = () => {
                    
                     </div>
                 ))}
+                <div className="absolute bottom-0 right-0  ">
+                    <Image 
+                     src= "/Primary Logo White@300x.png"
+                     width={100}
+                     height={100}
+                     className=" mixed-blend-overlay w-96 h-96"
+                     
+                     >
+                       
+                    </Image>
+
+                </div>
               
             </div>
        
